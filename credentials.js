fb2c8ca61a9406d6db53bbd25a225f22450400ae
anoxdleo <chothanimohit1@gmail.com>
2020-11-29 00:41:21 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,2 +1,2 @@
-export const API_KEY = "AIzaSyBdi6qX3xEaEwf-Y_BGcfPDaKwZ1FwmiXE"
-export const CLIENT_ID = "522253117288-1j6184hoivd6piclh0qduv4uu3bc2n6n.apps.googleusercontent.com"
+export const API_KEY = "AIzaSyBtbCTtOQ-nB57rS_zDO8iQsOUXDZFLrUo"
+export const CLIENT_ID = "584395958047-j2tvupv3sarcqq2m7kdlqhtoups3467o.apps.googleusercontent.com"
