434656ec4699528ad642a20dc8d413ad2c5a203d
anoxdleo <chothanimohit1@gmail.com>
2020-11-29 01:02:21 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,2 +1,2 @@
-export const API_KEY = "AIzaSyCp3dYbxqs4af_i4hpQJ4FHLGuYYbcow5s"
-export const CLIENT_ID = "174264315078-59s6jgh7r4vmbc0pum3u44d9kb291mqb.apps.googleusercontent.com"
+export const API_KEY = "AIzaSyDkL18ORjIbIOEQ3mg5eba-i39cyxK2aFA"
+export const CLIENT_ID = "665720525498-4ei9u4p6hs6k9mc3gujimguf6g1g01b8.apps.googleusercontent.com"
